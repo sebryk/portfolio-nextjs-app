ccc9ac0d2454caed6f54767ad8ed13e951143300
Sergei Brykalov <lastsebr@gmail.com>
2024-08-01 19:09:00 +0300

    initial commit

---
+++ src/app/components/Header/Nav/index.jsx
@@ -0,0 +1,11 @@
+import { Container } from "./Nav.css";
+const Nav = () => {
+  
+  return (  
+    <Container>
+      Hello
+    </Container>
+  );
+}
+ 
+export default Nav;
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+// import Nav from "./components/Header/Nav";
+export default function Home() {
+  return (
+    <main>
+      {/* <Nav/> */}
+      <h1>Hello</h1>
+    </main>
+  );
+}
