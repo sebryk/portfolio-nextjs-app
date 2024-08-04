290e217ddbb847726dbe3c83b5c56f3f88e868f0
Sergei Brykalov <lastsebr@gmail.com>
2024-08-04 20:54:48 +0300

    add nav component

--- src/app/components/Header/Nav/index.tsx
+++ src/app/components/Header/Nav/index.tsx
@@ -1,10 +1,27 @@
-import { Container } from "./Nav.css";
+import styles from "./Nav.module.scss";
 const Nav = () => {
   
   return (  
-    <Container>
-      Hello
-    </Container>
+    <nav className={styles["header__nav nav"]}>
+          <a href="#header" className={styles["nav__logo"]}>SB</a>
+          <div className={styles["nav__wrapper"]}>
+            <ul className={styles["nav__list"]}>
+              <li className={styles["nav__list-item"]}><a href="#about" className={styles["nav__list-link"]}>About</a></li>
+              <li className={styles["nav__list-item"]}><a href="#projects" className={styles["nav__list-link"]}>Projects</a>
+              </li>
+              <li className={styles["nav__list-item"]}><a href="#contacts" className={styles["nav__list-link"]}>Contact</a>
+              </li>
+            </ul>
+          </div>
+          <div className={styles["nav__language"]}>
+            <p className={styles["nav__language-curr"]}>EN</p>
+            <a className={styles["nav__language-link"]} href="./ru.html">RU</a>
+          </div>
+          <div className={styles["nav-icon icon"]}>
+            <span className={styles["icon__stripe"]}></span>
+              <span className={styles["icon__stripe"]}></span>
+          </div>
+        </nav>
   );
 }
  
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 import './styles.scss'
+import Nav from "./components/Header/Nav";
 export default function Home() {
   return (
+    <>
+    <Nav/>
     <main>
-      <h1>Hello</h1>
+      <h1>Hello </h1>
     </main>
+    </>
   );
 }
